64a5ccc59752d5a1d9024ca5fc1f933f2e81f1fc
sirily11 <sirily1997@gmail.com>
2023-05-30 10:23:57 +0800

    fix: add Post and Get decorators

--- src/app/api/auth/challenge/route.ts
+++ src/app/api/auth/challenge/route.ts
@@ -1,16 +1,24 @@
+import { Post } from "@/decorators/nextresponse.decorator";
 import { PasswordlessServerService } from "@/services/Passwordless.service.server";
 
 export const runtime = "edge";
 
-export async function POST(request: Request) {
-  const { searchParams, origin } = new URL(request.url);
-  const domain: any = origin.replace(/^https?:\/\//, "");
-  const userId: any = searchParams.get("userId");
-  const type: any = searchParams.get("type");
+class ChallengeRoute {
+  @Post()
+  static createChallenge(request: Request) {
+    const { searchParams, origin } = new URL(request.url);
+    const domain: any = origin.replace(/^https?:\/\//, "");
+    const userId: any = searchParams.get("userId");
+    const type: any = searchParams.get("type");
 
-  return PasswordlessServerService.getChallenge({
-    type,
-    userId,
-    domain,
-  });
+    return PasswordlessServerService.getChallenge({
+      type,
+      userId,
+      domain,
+    });
+  }
 }
+
+const POST = ChallengeRoute.createChallenge;
+
+export { POST };
---
+++ src/decorators/nextresponse.decorator.ts
@@ -0,0 +1,37 @@
+import { NextResponse } from "next/server";
+
+export function Post() {
+  return function (
+    target: any,
+    propertyKey: string,
+    descriptor: PropertyDescriptor
+  ) {
+    const originalMethod = descriptor.value;
+    descriptor.value = async function (...args: any[]) {
+      const result = await originalMethod.apply(this, args);
+      if (result instanceof NextResponse) {
+        return result;
+      }
+      return NextResponse.json(result, { status: 201 });
+    };
+    return descriptor;
+  };
+}
+
+export function Get() {
+  return function (
+    target: any,
+    propertyKey: string,
+    descriptor: PropertyDescriptor
+  ) {
+    const originalMethod = descriptor.value;
+    descriptor.value = async function (...args: any[]) {
+      const result = await originalMethod.apply(this, args);
+      if (result instanceof NextResponse) {
+        return result;
+      }
+      return NextResponse.json(result, { status: 200 });
+    };
+    return descriptor;
+  };
+}
