36a2d8266149bacced1b96116e04e93d3e0b5424
sirily11 <sirily1997@gmail.com>
2023-05-30 16:25:55 +0800

    fix: runtime

--- src/app/api/auth/challenge/route.ts
+++ src/app/api/auth/challenge/route.ts
@@ -1,24 +1,7 @@
-import { Post } from "@/decorators/nextresponse.decorator";
-import { PasswordlessServerService } from "@/services/Passwordless.service.server";
+import { ChallengeController } from "@/controllers/Challenge.controller";
 
 export const runtime = "edge";
 
-class ChallengeRoute {
-  @Post()
-  static createChallenge(request: Request) {
-    const { searchParams, origin } = new URL(request.url);
-    const domain: any = origin.replace(/^https?:\/\//, "");
-    const userId: any = searchParams.get("userId");
-    const type: any = searchParams.get("type");
-
-    return PasswordlessServerService.getChallenge({
-      type,
-      userId,
-      domain,
-    });
-  }
+export async function POST(request: Request) {
+  return ChallengeController.createChallenge(request);
 }
-
-const POST = ChallengeRoute.createChallenge;
-
-export { POST };
