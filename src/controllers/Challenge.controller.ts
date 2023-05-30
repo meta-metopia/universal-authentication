9f672f5fd7d5f3d6c8af5f86f0f1b6e9528c5cca
sirily11 <32106111+sirily11@users.noreply.github.com>
2023-05-30 16:43:18 +0800

    fix: runtime (#3)

---
+++ src/controllers/Challenge.controller.ts
@@ -0,0 +1,18 @@
+import { Post } from "@/decorators/nextresponse.decorator";
+import { PasswordlessServerService } from "@/services/Passwordless.service.server";
+
+export class ChallengeController {
+  @Post()
+  static createChallenge(request: Request) {
+    const { searchParams, origin } = new URL(request.url);
+    const domain: any = origin.replace(/^https?:\/\//, "");
+    const userId: any = searchParams.get("userId");
+    const type: any = searchParams.get("type");
+
+    return PasswordlessServerService.getChallenge({
+      type,
+      userId,
+      domain,
+    });
+  }
+}
